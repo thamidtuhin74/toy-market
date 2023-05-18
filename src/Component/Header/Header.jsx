9df2a36e014e6ab36c447ac5c153a174c3550cb0
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2023-05-19 02:45:35 +0600

    Initialize client site & setup route & tailwind

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            This is Hader
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            THis is home
+        </div>
+    );
+};
+
+export default Home;
